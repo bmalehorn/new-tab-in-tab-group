b0999ca0cd53669eb97062df1a19fa0836795ac2
Brian Malehorn <bmalehorn@gmail.com>
2022-06-29 22:04:31 -0400

    update for manifest v3, remove extraneous code

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
-chrome.commands.onCommand.addListener(function (command) {
+chrome.commands.onCommand.addListener(function () {
   chrome.tabs.query(
     { active: true, currentWindow: true },
     function (currentTabs) {
-      var curr = currentTabs[0];
-      chrome.tabs.create({ index: curr.index + 1 }, function (newTab) {
-        chrome.tabs.group({ tabIds: newTab.id, groupId: curr.groupId });
+      var currentTab = currentTabs[0];
+      chrome.tabs.create({ index: currentTab.index + 1 }, function (newTab) {
+        // no current group
+        if (currentTab.groupId === -1) {
+          return;
+        }
+        chrome.tabs.group({ tabIds: newTab.id, groupId: currentTab.groupId });
       });
     }
   );
