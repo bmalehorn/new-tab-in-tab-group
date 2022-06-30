455248ad10aed7a00e2c42ad4f4fc4e554eccd82
Brian Malehorn <bmalehorn@gmail.com>
2022-06-29 21:44:24 -0400

    prettier

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
-chrome.commands.onCommand.addListener(function(command) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(currentTabs) {
-    var curr = currentTabs[0];
-    chrome.tabs.create({index: curr.index+1}, function(newTab) {
-      chrome.tabs.group({tabIds: newTab.id, groupId: curr.groupId});
-    });
-  });
+chrome.commands.onCommand.addListener(function (command) {
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    function (currentTabs) {
+      var curr = currentTabs[0];
+      chrome.tabs.create({ index: curr.index + 1 }, function (newTab) {
+        chrome.tabs.group({ tabIds: newTab.id, groupId: curr.groupId });
+      });
+    }
+  );
 });
