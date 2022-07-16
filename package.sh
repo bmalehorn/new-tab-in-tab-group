c435e1e749afcb2bc4d411500a54e5bf03a3e831
Brian Malehorn <bmalehorn@gmail.com>
2022-07-16 16:23:26 -0700

    update package.sh to remove old .zip instead of adding to it

--- package.sh
+++ package.sh
@@ -4,4 +4,5 @@ set -xeuo pipefail
 # cd to directory of script
 cd "$(dirname "$0")"
 cd ../
+rm -f new-tab-in-tab-group.zip
 zip -r new-tab-in-tab-group.zip new-tab-in-tab-group/{background.js,manifest.json}
