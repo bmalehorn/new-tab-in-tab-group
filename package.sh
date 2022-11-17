18af9ff999722798a1ba03b13fdc7b4b00d3ac1a
Brian Malehorn <bmalehorn@gmail.com>
2022-11-16 21:59:05 -0800

    downsize icon

---
+++ package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -xeuo pipefail
+# cd to directory of script
+cd "$(dirname "$0")"
+cd ../
+rm -f new-tab-in-tab-group.zip
+zip -r new-tab-in-tab-group.zip new-tab-in-tab-group/{background.js,manifest.json,icon.png}
+mv new-tab-in-tab-group.zip new-tab-in-tab-group/
